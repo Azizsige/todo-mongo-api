fc0adad10fe274b532ec537c876447f20042e6f8
azizsige <kurniawan00azizsige@gmail.com>
2023-06-16 12:36:24 +0700

    upate: cors local

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://todo-mongo.vercel.app",
+    origin: ["https://todo-mongo.vercel.app", "http://localhost:1234/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
