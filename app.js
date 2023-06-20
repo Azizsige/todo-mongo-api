87fa41eba8544f9dbd2c499f09fe1b3e0d2cce06
azizsige <kurniawan00azizsige@gmail.com>
2023-06-20 17:04:34 +0700

    update: allow all host

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:1234",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
