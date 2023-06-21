e817ce11d22c04ec6774787b605971dc35484226
azizsige <kurniawan00azizsige@gmail.com>
2023-06-21 19:54:55 +0700

    change cors origin

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:1234",
+    origin: "https://todo-mongo.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
