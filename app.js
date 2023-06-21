7253daf1cf889237cdfd3943c0ee4bd532ab22eb
azizsige <kurniawan00azizsige@gmail.com>
2023-06-21 19:21:04 +0700

    update: turn on for testing cookie and ui mobile

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:1234",
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
