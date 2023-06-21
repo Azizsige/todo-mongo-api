8a464052eace93c038a46e7db1f5d4fa23d9abe4
azizsige <kurniawan00azizsige@gmail.com>
2023-06-21 19:29:31 +0700

    update: turn on for testing cookie and ui mobile

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://todo-mongo.vercel.app/",
+    origin: ["https://todo-mongo.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
