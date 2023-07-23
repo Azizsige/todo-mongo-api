0f787f534cdca6102586eac1e97e8971efbe798b
azizsige <kurniawan00azizsige@gmail.com>
2023-07-23 20:01:03 +0700

    update: rule in routes put user update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 const userController = require("../controllers/userController");
 
+const { body } = require("express-validator");
+
 router.get(
   "/me",
   authMiddleware.authenticateToken,
@@ -12,6 +14,40 @@ router.get(
 
 router.put(
   "/update/:id",
+  [
+    // Validasi field harus diisi
+    body("username").custom((value, { req }) => {
+      //  check if input using space or not
+      if (!value) {
+        throw new Error("Username harus diisi");
+      } else if (value.match(/\s/g)) {
+        throw new Error("Username tidak boleh menggunakan spasi");
+      }
+      return true;
+    }),
+    body("email").custom((value, { req }) => {
+      //  check email harus pakai @
+      if (!value) {
+        throw new Error("Email harus diisi huhuh");
+      }
+
+      // check email harus @gmail atau @yahoo
+      if (!value.match(/^[^\s@]+@(gmail|yahoo)\.com$/)) {
+        throw new Error("Email tidak valid");
+      }
+      return true;
+    }),
+    body("password").custom((value, { req }) => {
+      // cek jika password kosong
+      if (!value) {
+        throw new Error("Password harus diisi");
+      } else if (value.length < 6) {
+        // cek jika panjang password kurang dari 6 karakter
+        throw new Error("Password minimal 6 karakter");
+      }
+      return true;
+    }),
+  ],
   authMiddleware.authenticateToken,
   userController.updateUser
 );
